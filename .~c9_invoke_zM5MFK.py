d7576b74454c3c6a268bce7a048984f45db5fc43
EC2 Default User <ec2-user@ip-10-0-2-207.us-west-2.compute.internal>
2025-03-05 05:29:14 +0000

    new

---
+++ .~c9_invoke_zM5MFK.py
@@ -0,0 +1,8 @@
+#config.py
+AWS_CONFIG = {
+    'region_name': 'us-west-2',
+    'embedding_model_id': 'amazon.titan-embed-text-v2:0',
+    'chat_model_id': 'us.anthropic.claude-3-5-sonnet-20241022-v2:0',
+    'inference_profile_arn': 'arn:aws:bedrock:us-west-2:171799616799:inference-profile/us.anthropic.claude-3-5-sonnet-20241022-v2:0',
+    'collection_endpoint': 'https://gwkgoux94kcse5ni87gf.us-west-2.aoss.amazonaws.com'
+}
--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #config.py
 AWS_CONFIG = {
-    'region_name': 'ap-northeast-2',
+    'region_name': 'us-west-2',
     'embedding_model_id': 'amazon.titan-embed-text-v2:0',
-    'chat_model_id': 'apac.anthropic.claude-3-5-sonnet-20240620-v1:0',
+    'chat_model_id': 'us.anthropic.claude-3-5-sonnet-20241022-v2:0',
     'inference_profile_arn': '',
     'collection_endpoint': ''
 }
